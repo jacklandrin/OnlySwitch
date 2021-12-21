a857c52d4cdd79e97c8858faa5612ba93aeadfe6
Jacklandrin <jacklandrin@hotmail.com>
2021-12-21 22:09:37 +0100

    xcode install back to 13.2

--- OnlySwitch/AppDelegate.swift
+++ OnlySwitch/AppDelegate.swift
@@ -14,11 +14,10 @@ struct OnlySwitchApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
-            EmptyView().frame(width: 10, height: 10)
+            EmptyView().frame(width: 0, height: 0)
         }
     }
 }
-
 
 class AppDelegate:NSObject, NSApplicationDelegate {
     var statusBar: StatusBarController?
