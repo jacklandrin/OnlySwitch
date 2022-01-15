e7aab497b94b3571749549d99cc90b1e4d7d64ee
jack <jacklandrin@hotmail.com>
2022-01-15 17:10:47 +0100

    resolve warning

--- OnlySwitch/View/Setting/Shortcuts/ShortcutsSettingVM.swift
+++ OnlySwitch/View/Setting/Shortcuts/ShortcutsSettingVM.swift
@@ -58,36 +58,39 @@ class ShortcutsSettingVM:ObservableObject {
     @Published var errorInfo = ""
     @Published var showErrorToast = false
     func loadShortcutsList() {
-        let result = getShortcutsList.runAppleScript(isShellCMD: true)
-        if result.0 {
-            let allshortcuts = (result.1 as! String).split(separator: "\r")
-            let shortcutsDic = UserDefaults.standard.dictionary(forKey: shorcutsDicKey)
-            var newShortcutsDic:[String:Bool] = [String:Bool]()
-            if let shortcutsDic = shortcutsDic {
-                self.shortcutsList = [ShorcutsItem]()
-                for name in allshortcuts {
-                    if let toggle = shortcutsDic[String(name)] as? Bool {
-                        self.addItem(name: String(name), toggle: toggle)
-                        newShortcutsDic[String(name)] = toggle
-                    } else {
-                        self.addItem(name: String(name), toggle: false)
+        DispatchQueue.main.async {
+            let result = getShortcutsList.runAppleScript(isShellCMD: true)
+            if result.0 {
+                let allshortcuts = (result.1 as! String).split(separator: "\r")
+                let shortcutsDic = UserDefaults.standard.dictionary(forKey: shorcutsDicKey)
+                var newShortcutsDic:[String:Bool] = [String:Bool]()
+                if let shortcutsDic = shortcutsDic {
+                    self.shortcutsList = [ShorcutsItem]()
+                    for name in allshortcuts {
+                        if let toggle = shortcutsDic[String(name)] as? Bool {
+                            self.addItem(name: String(name), toggle: toggle)
+                            newShortcutsDic[String(name)] = toggle
+                        } else {
+                            self.addItem(name: String(name), toggle: false)
+                            newShortcutsDic[String(name)] = false
+                        }
+                    }
+                } else {
+                    self.shortcutsList = allshortcuts.map{ ShorcutsItem(name: String($0), toggle: false, error: {[weak self] info in
+                        guard let strongSelf = self else {return}
+                        strongSelf.errorInfo = info
+                        strongSelf.showErrorToast = true
+                    }) }
+                    for name in allshortcuts {
                         newShortcutsDic[String(name)] = false
                     }
                 }
-            } else {
-                self.shortcutsList = allshortcuts.map{ ShorcutsItem(name: String($0), toggle: false, error: {[weak self] info in
-                    guard let strongSelf = self else {return}
-                    strongSelf.errorInfo = info
-                    strongSelf.showErrorToast = true
-                }) }
-                for name in allshortcuts {
-                    newShortcutsDic[String(name)] = false
-                }
+                
+                UserDefaults.standard.set(newShortcutsDic, forKey: shorcutsDicKey)
+                UserDefaults.standard.synchronize()
             }
-            
-            UserDefaults.standard.set(newShortcutsDic, forKey: shorcutsDicKey)
-            UserDefaults.standard.synchronize()
         }
+        
     }
     
     func addItem(name:String, toggle:Bool) {
