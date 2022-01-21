6856bf47dbb1f7dcbd02d21917814bff1ede80a3
jack <jacklandrin@hotmail.com>
2022-01-21 01:06:11 +0100

    modify description about set dark mode intent

--- IntentsExtension/Actions/SetDarkMode.swift
+++ IntentsExtension/Actions/SetDarkMode.swift
@@ -12,13 +12,10 @@ final class SetDarkModeIntentHandler:NSObject, SetDarkModeIntentHandling {
     func handle(intent: SetDarkModeIntent) async -> SetDarkModeIntentResponse {
         let response = SetDarkModeIntentResponse(code: .success, userActivity: nil)
         if intent.State?.boolValue == true {
-//            UserDefaults.standard.set("Light", forKey: "AppleInterfaceStyle")
             _ = turnOnDarkModeCMD.runAppleScript()
         } else {
-//            UserDefaults.standard.set("Dark", forKey: "AppleInterfaceStyle")
            _ = turnOffDarkModeCMD.runAppleScript()
         }
-//        UserDefaults.standard.synchronize()
         return response
     }
 }
