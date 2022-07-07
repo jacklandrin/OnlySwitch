0dd1ffcffe5784a6d9bbaa22f324c7cabea96c95
jack <jacklandrin@hotmail.com>
2022-07-07 10:07:11 +0200

    fix bug: after closing screen test, the switch status didn't update

--- OnlySwitch/View/PureColorView/PureColorVM.swift
+++ OnlySwitch/View/PureColorView/PureColorVM.swift
@@ -75,9 +75,10 @@ class PureColorVM:ObservableObject {
     func exitScreenTestMode() {
         Task {
             try? await ScreenTestSwitch.shared.operationSwitch(isOn: false)
+            DispatchQueue.main.async {
+                NotificationCenter.default.post(name: .refreshSingleSwitchStatus, object: SwitchType.screenTest)
+            }
         }
-        
-        NotificationCenter.default.post(name: .refreshSingleSwitchStatus, object: SwitchType.screenTest)
     }
     
 }
