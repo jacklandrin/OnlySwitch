6929155e9d3d17c3ae179371b4a115ab10f4fa3e
jack <jacklandrin@hotmail.com>
2022-08-07 01:23:09 +0200

    increase setting window height

--- OnlySwitch/Define/LayoutDefine.swift
+++ OnlySwitch/Define/LayoutDefine.swift
@@ -13,7 +13,7 @@ struct Layout{
     static let soundWaveWidth:CGFloat = Layout.popoverWidth
     static let soundWaveHeight:CGFloat = 150
     static let settingWindowHeight:CGFloat = 400
-    static let settingWindowWidth:CGFloat = 900
+    static let settingWindowWidth:CGFloat = 920
     static let singleSwitchHeight:CGFloat = 38.0
     static let generalSettingItemHeight:CGFloat = 30.0
     static let generalSettingSpacing:CGFloat = 7.0
