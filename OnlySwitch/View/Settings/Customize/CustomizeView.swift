2b05aacd53ad628cd6769d35e0d1bc6c12c9d28d
jack <jacklandrin@hotmail.com>
2022-12-19 21:20:04 +0100

    change List to LazyVStack

--- OnlySwitch/View/Settings/Customize/CustomizeView.swift
+++ OnlySwitch/View/Settings/Customize/CustomizeView.swift
@@ -17,22 +17,26 @@ struct CustomizeView: View {
             Text("To add or remove any switches on list".localized())
                 .padding(10)
             Divider()
-            List {
-                ForEach(customizeVM.allSwitches.indices, id:\.self) { index in
-                    HStack {
-                        Toggle("", isOn: $customizeVM.allSwitches[index].toggle)
-                        Image(nsImage: barInfo(index: index).onImage!.resizeMaintainingAspectRatio(withSize: NSSize(width: 50, height: 50))!)
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 25, height: 25)
-                        Text(barInfo(index:index).title.localized())
-                            .frame(width:170, alignment: .leading)
-                            .padding(.trailing, 10)
-                        KeyboardShortcuts.Recorder(for: customizeVM.allSwitches[index].keyboardShortcutName)
-                            .environment(\.locale, .init(identifier: langManager.currentLang))//Localizable doesn't work
-                    }                    
+            ScrollView {
+                LazyVStack{
+                    ForEach(customizeVM.allSwitches.indices, id:\.self) { index in
+                        HStack {
+                            Toggle("", isOn: $customizeVM.allSwitches[index].toggle)
+                            Image(nsImage: barInfo(index: index).onImage!.resizeMaintainingAspectRatio(withSize: NSSize(width: 50, height: 50))!)
+                                .renderingMode(.template)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                            Text(barInfo(index:index).title.localized())
+                                .frame(width:170, alignment: .leading)
+                                .padding(.trailing, 10)
+                            KeyboardShortcuts.Recorder(for: customizeVM.allSwitches[index].keyboardShortcutName)
+                                .environment(\.locale, .init(identifier: langManager.currentLang))//Localizable doesn't work
+                            Spacer()
+                        }.padding(.leading)
+                    }
                 }
+               
             }
             Divider()
                 .padding(.bottom,10)
