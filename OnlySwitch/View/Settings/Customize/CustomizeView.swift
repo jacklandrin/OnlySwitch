a5978cda10fcb6b26248168a5665eca8edf8862c
jack <jacklandrin@hotmail.com>
2023-01-18 21:11:22 +0100

    add switches availability on customize settings

--- OnlySwitch/View/Settings/Customize/CustomizeView.swift
+++ OnlySwitch/View/Settings/Customize/CustomizeView.swift
@@ -12,6 +12,9 @@ import KeyboardShortcuts
 struct CustomizeView: View {
     @ObservedObject var customizeVM = CustomizeVM.shared
     @ObservedObject var langManager = LanguageManager.sharedManager
+    @ObservedObject var preferencesvm = PreferencesObserver.shared
+    @State var preferences = PreferencesObserver.shared.preferences
+    
     var body: some View {
         VStack(alignment:.leading) {
             Text("To add or remove any switches on list".localized())
@@ -31,7 +34,37 @@ struct CustomizeView: View {
                                 .frame(width:170, alignment: .leading)
                                 .padding(.trailing, 10)
                             KeyboardShortcuts.Recorder(for: customizeVM.allSwitches[index].keyboardShortcutName)
-                                .environment(\.locale, .init(identifier: langManager.currentLang))//Localizable doesn't work
+                                .environment(\.locale, .init(identifier: langManager.currentLang))//Localizable follow system
+                            Group {
+                                if customizeVM.allSwitches[index].type == .hideMenubarIcons {
+                                    Button(action: {
+                                        preferencesvm.preferences.menubarCollaspable = !preferences.menubarCollaspable
+                                    }, label: {
+                                        if preferencesvm.preferences.menubarCollaspable {
+                                            Text("Disable".localized())
+                                        } else {
+                                            Text("Enable".localized())
+                                        }
+                                    })
+                                } else if customizeVM.allSwitches[index].type == .radioStation || customizeVM.allSwitches[index].type == .backNoises {
+                                    Button(action: {
+                                        preferencesvm.preferences.radioEnable = !preferences.radioEnable
+                                        if preferences.radioEnable {
+                                            PlayerManager.shared.player.setupRemoteCommandCenter()
+                                        } else {
+                                            RadioStationSwitch.shared.playerItem.isPlaying = false
+                                            PlayerManager.shared.player.clearCommandCenter()
+                                        }
+                                    }, label: {
+                                        if preferencesvm.preferences.radioEnable {
+                                            Text("Disable".localized())
+                                        } else {
+                                            Text("Enable".localized())
+                                        }
+                                    })
+                                }
+                            }.padding(.leading, 10)
+                            
                             Spacer()
                         }.padding(.leading)
                     }
