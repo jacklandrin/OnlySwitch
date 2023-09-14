95df508787bb61d259d1f8212007b86b9faf183c
jack <jacklandrin@hotmail.com>
2023-09-14 14:32:13 +0200

    change indentation

--- OnlySwitch/View/Settings/Evolution/EvolutionEditorView.swift
+++ OnlySwitch/View/Settings/Evolution/EvolutionEditorView.swift
@@ -125,17 +125,18 @@ struct EvolutionEditorView: View {
             .onAppear{
 
             }
-            .toast(isPresenting: viewStore.binding(
-                get: { $0.showError },
-                send: { .errorControl($0) }
-            ),
-                   alert: {
-                AlertToast(
-                    displayMode: .alert,
-                    type: .error(.red),
-                    title: "Save commands failed.".localized()
-                )
-            }
+            .toast(
+                isPresenting: viewStore.binding(
+                    get: { $0.showError },
+                    send: { .errorControl($0) }
+                ),
+                alert: {
+                    AlertToast(
+                        displayMode: .alert,
+                        type: .error(.red),
+                        title: "Save commands failed.".localized()
+                    )
+                }
             )
         }
     }
