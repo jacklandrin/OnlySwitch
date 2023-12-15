35360df8259e1129fd94b2394e4ae90ef5a988ac
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2023-12-15 15:02:22 +0800

    Add view, reducer, tests to refactor BackNoiesesSetting

---
+++ OnlySwitch/View/Settings/BackNoises/BackNoisesSettingReducer.swift
@@ -0,0 +1,8 @@
+//
+//  BackNoisesSettingReducer.swift
+//  OnlySwitch
+//
+//  Created by Leon on 2023/12/15.
+//
+
+import Foundation
---
+++ OnlySwitch/View/Settings/BackNoises/BackNoisesSettingTCAView.swift
@@ -0,0 +1,18 @@
+//
+//  BackNoisesSettingTCAView.swift
+//  OnlySwitch
+//
+//  Created by Leon on 2023/12/15.
+//
+
+import SwiftUI
+
+struct BackNoisesSettingTCAView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackNoisesSettingTCAView()
+}
